7441ee21a2df2ce797ecfda39c1a309cae621980
Jacob <hello@jacobbinnie.com>
2023-08-18 19:37:26 +1200

    feat: initial push

---
+++ src/components/LandingInfo/index.tsx
@@ -0,0 +1,17 @@
+interface LandingInfoProps {}
+
+function LandingInfo({}: LandingInfoProps) {
+  return (
+    <div className="flex flex-col max-w-3xl items-center gap-5">
+      <h1 className="text-3xl sm:text-6xl tracking-tighter text-center font-bold transition-all">
+        Brand Name Checker Thingy
+      </h1>
+      <p className="tracking-tighter text-center w-full max-w-[500px] transition-all">
+        Check domain name, social handle & registered business name availability
+        for your next project. Free to use, forever.
+      </p>
+    </div>
+  );
+}
+
+export default LandingInfo;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        "black-swoosh": "black-swoosh 10s ease infinite",
+      },
+      keyframes: {
+        "black-swoosh": {
+          "0%": {
+            "background-size": "600% 100%",
+            "background-position": "left center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+          "50%": {
+            "background-size": "600% 100%",
+            "background-position": "right center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+          "100%": {
+            "background-size": "600% 100%",
+            "background-position": "left center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+        },
+      },
+      fontSize: {
+        small: "16px",
+      },
+      colors: {
+        primary: "#0a0a0a",
+        secondary: "#6b7280",
+        tertiary: "#f5f5f5",
+        accent: "#FFA500",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
