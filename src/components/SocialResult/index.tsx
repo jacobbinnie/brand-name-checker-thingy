cf8a9254549dbc9d674d707427066ac0533bfc21
Alexander Nicholson <alexn400@gmail.com>
2023-08-18 20:58:19 +1200

    Add .idea to gitignore

--- src/components/SocialResult/index.tsx
+++ src/components/SocialResult/index.tsx
@@ -1,8 +1,10 @@
-interface SocialResultProps {}
+interface SocialResultProps {
 
-function SocialResult({}: SocialResultProps)  {
-    return <div className={"bg-white rounded-lg p-4"}>
+}
 
+function SocialResult({}: SocialResultProps) {
+    return <div className={"bg-gray-900 rounded-lg p-4 flex flex-col "}>
+        <h2 className={'text-center text-2xl font-semibold'}>Socials</h2>
     </div>
 }
 
