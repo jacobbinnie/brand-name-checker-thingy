28fb75b96979268866c4325966b546f8b83c9b68
Alexander Nicholson <alexn400@gmail.com>
2023-08-18 20:45:09 +1200

    Setup social results component

---
+++ src/components/SocialResult/index.tsx
@@ -0,0 +1,9 @@
+interface SocialResultProps {}
+
+function SocialResult({}: SocialResultProps)  {
+    return <div className={"bg-white rounded-lg p-4"}>
+
+    </div>
+}
+
+export default SocialResult;
