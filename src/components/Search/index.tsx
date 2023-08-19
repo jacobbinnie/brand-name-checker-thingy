602551830f5bdf36bf615605f2e284600599c4f7
Jacob <hello@jacobbinnie.com>
2023-08-19 15:20:04 +1200

    feat: add loading state to search button

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -34,7 +34,7 @@ function Search({
         <ArrowPathIcon
           className={clsx(
             loading ? "block" : "hidden",
-            "w-5 h-5 absolute right-5 top-5 animate-spin text-tertiary ml-2",
+            "w-5 h-5 absolute right-5 top-5 animate-spin text-tertiary ml-2"
           )}
         />
       </div>
@@ -54,21 +54,27 @@ function Search({
         disabled={searchQuery.length < 3}
         className="w-full h-12 bg-accent disabled:bg-gray-700 transition-all text-primary font-semibold tracking-tighter flex items-center justify-center rounded-lg max-w-[400px]"
       >
-        Search
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={3}
-          stroke="currentColor"
-          className="w-4 h-4 ml-2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
+        {loading ? (
+          <ArrowPathIcon className="w-5 h-5 text-tertiary animate-spin" />
+        ) : (
+          <>
+            Search
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="currentColor"
+              className="w-4 h-4 ml-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </>
+        )}
       </button>
     </div>
   );
