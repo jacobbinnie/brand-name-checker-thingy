8c4a6ec8ff09c5764f4b411239a2cf51c2fb421e
Alexander Nicholson <alexn400@gmail.com>
2023-08-19 15:03:22 +1200

    Update color scheme

--- src/components/SocialResult/index.tsx
+++ src/components/SocialResult/index.tsx
@@ -33,10 +33,28 @@ function SocialResult({ search }: SocialResultProps) {
 
   return (
     <div className={"bg-gray-800 rounded-lg p-4 flex flex-col"}>
-      <h2 className={"text-center text-2xl font-semibold"}>Socials</h2>
-      <p
-        className={"text-center text-gray-300 font-semibold"}
-      >{`@${search?.query}`}</p>
+      <div className={"flex items-center justify-center gap-2 pb-2"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-6 h-6 text-accent"
+        >
+          <path
+            strokeLinecap="round"
+            d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
+          />
+        </svg>
+        <h2 className={"text-center text-2xl font-semibold"}>Socials</h2>
+      </div>
+
+      {search?.query && (
+        <p
+          className={"text-center text-gray-300 font-semibold"}
+        >{`@${search?.query}`}</p>
+      )}
       {data && (
         <table>
           <tbody>
