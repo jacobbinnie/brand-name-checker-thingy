bcd2d196153863c013e3116fb2c130c9923d338a
Alexander Nicholson <alexn400@gmail.com>
2023-08-19 12:35:59 +1200

    Add cache storage for fetching social availability

---
+++ src/app/api/check-socials/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import { kv } from "@vercel/kv";
+import { SocialPlatform } from "@/app/interfaces/socialPlatforms";
+
+const RAPID_API_KEY = process.env.NEXT_PUBLIC_RAPID_API_KEY || "";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const q = searchParams.get("q");
+
+  const socials = await kv.get<Record<SocialPlatform, boolean>>(`${q}-socials`);
+
+  if (socials) {
+    console.log("Fetched from cache");
+    // return from cache
+    return NextResponse.json(socials);
+  } else {
+    console.log("Cache miss - fetch from api");
+    const options = {
+      method: "GET",
+      url: `https://check-username.p.rapidapi.com/check/${q}`,
+      headers: {
+        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Host": "check-username.p.rapidapi.com",
+      },
+    };
+
+    const { data } = await axios(options);
+
+    const { name, success, ...rest } = data;
+
+    await kv.set<Record<SocialPlatform, boolean>>(`${q}-socials`, rest);
+
+    return NextResponse.json(rest);
+  }
+}
